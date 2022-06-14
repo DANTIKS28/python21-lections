5d3273b2f141cb91057d711ce5a5f81ad41aa34d
DANTIKS28 <danielamanov28@gmail.com>
2022-06-14 12:47:24 +0600

    dictionar.py bhb nb

--- dictionar.py
+++ dictionar.py
@@ -11,12 +11,28 @@ print(dict_["a"]) # Hello
 dict_ = {
     1:"hello", 
     4.5: {"a":5},
-    # {"s":5}: 44  # TypeError: unhashable type: 'dict' 
 }
+    # {"s":5}: 44  # TypeError: unhashable type: 'dict' 
 
-print(dict_[4.5]) # {"a":5}
-print(dict_[4.5]["a"]) # 5
-# print(dict_["a"]) # KeyError: "a"
+"=============Создание словарей================"
+dict1 = {"a":3}
+dict2 = dict( [ ('key1', 'value1'), ('key2', 'value2') ] )
+# dict2 = {'key1':'value1', 'key2':'value2'}
+dict3 = dict( ( ['key1', 'value1'], ('key2', 'value2') ) )
+# dict3 = {'key1':'value1', 'key2':'value2'}
+dict4 = dict(['ab', 'cd', 'de'])
+# dict4 = {"a":"b", 'c':'d', 'd':'e'}
+key1, value1 = 'ab'
+dict4[key1] = value1
+key2, value2 = 'cd'
+dict4[key2] = value2
+key3, value3 = 'de'
+dict4[key3] = value3
+
+
+dict5 = dict(['abc']) # ValueError: dictionary update sequence element #0 has length 3; 2 is required
+key1, value1 = 'abc' #
+dict5[key1] = value1
 
 dict_ = {"a":2, "a":3, "a":4}
 print(dict_) # {"a":4}
